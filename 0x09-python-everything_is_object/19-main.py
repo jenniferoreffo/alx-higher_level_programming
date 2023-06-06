87dbe6d95b0261b68c138ca99a401bd15986c82c
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-06-06 20:30:35 +0000

    commit

--- 0x09-python-everything_is_object/19-main.py
+++ 0x09-python-everything_is_object/19-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 copy_list = __import__('19-copy_list').copy_list
+
 my_list = [1,2,3]
 print(my_list)
 
@@ -8,5 +9,5 @@ new_list = copy_list(my_list)
 print(my_list)
 print(new_list)
 
-print(new_list==my_list)
+print(new_list == my_list)
 print(new_list is my_list)
