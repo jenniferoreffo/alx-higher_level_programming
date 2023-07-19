d4805703bab53920f6c6dd7bcaddd263bb6dd8c0
jennyoreffo <jennyoreffo@gmail.com>
2023-07-19 15:08:19 +0000

    My Commit

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,7 +3,12 @@
 -- The user_0d_2 password should be ser to user_0d_2_pwd-- If the db hbtn_0d_2 already exists, the sceipt should not fail
 -- If the user user_0d_2 already exists, the sript should not fail. 
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2' .* TO 'user_0d_2'@'localhost';
+CREATE DATABASE
+    IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+ ON 'hbtn_0d_2'.*
+ TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
