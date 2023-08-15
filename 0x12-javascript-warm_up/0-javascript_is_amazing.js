c9167e99baac8cb3f35f84fc0b67841cc6f50cec
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-08-15 07:17:58 +0000

    Commit

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -2,5 +2,5 @@
 
 //A script that prints Javascript is amazing
 
-const myVar = 'javascript is amazing';
+const myVar = 'Javascript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,5 +3,5 @@
 //A script that prints 3lines 
 //1. C is fun, 2. Python is cool, 3. javascript
 
-const myVar = 'C is fun\npython is cool\njavascript is amazing';
+const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
 console.log(myVar);
