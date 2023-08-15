6c8640b0daa432c4a1f7e63d5e16c5d45d322710
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-08-15 22:52:03 +0000

    Commit

--- 0x13-javascript_objects_scopes_closures/tests/2-main.js
+++ 0x13-javascript_objects_scopes_closures/tests/2-main.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const Rectangle = require('./2-rectangle');
 
-const r1 = new Rectangle();
+const r1 = new Rectangle(2, 3);
 console.log(r1);
-console.log(r1.constructor);
+console.log(r1.width);
+console.log(r1.height);
 
 const r2 = new Rectangle(2, -3);
 console.log(r2);
