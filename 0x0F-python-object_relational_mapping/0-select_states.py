52613cf4324d07e46b47d4f2e3bbcf30c7d4f64d
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-08-21 17:27:19 +0000

    my commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ A script that lists all states from the db 'hbtn_0e_0_usa' """
 
 if __name__ == '__ main__'
@@ -7,9 +8,11 @@ import MySQLdb
 from sys import argv
 
 """ Access to the db and get the states fron the db """
+
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwrd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute('SELECT id, name  FROM states ORDER BY id;')
+
     rows = cur.fetchall()
     for row in rows:
         print(f"{rows}")
