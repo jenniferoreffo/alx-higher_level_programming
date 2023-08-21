e34d48a242f8387ea78e1f7a276f8e33cab6a8a9
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-08-21 00:29:08 +0000

    Commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """ A script that lists all states from the db 'hbtn_0e_0_usa' """
 
+if __name__ == '__ main__'
+
 import MySQLdb
 from sys import argv
 
-if __name__ == '__ main__' 
 """ Access to the db and get the states fron the db """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwrd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute('SELECT id, name  FROM states ORDER BY id;')
     rows = cur.fetchall()
     for row in rows:
-        print(rows)
+        print(f"{rows}")
+    cur.close()
+    db.close()
